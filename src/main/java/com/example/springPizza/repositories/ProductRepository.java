7862b3a647b09ccffe5ae7a7d2e7a8f80192ea74
maydek1 <maydekcraft@gmail.com>
2024-06-20 20:31:22 +0300

    Added annotations @Repository

--- src/main/java/com/example/springPizza/repositories/ProductRepository.java
+++ src/main/java/com/example/springPizza/repositories/ProductRepository.java
@@ -2,9 +2,10 @@ package com.example.springPizza.repositories;
 
 import com.example.springPizza.database.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAllByCategoryId(Long categoryId);
 }
