a512676b42bbe12314ca63342326df1d84b594b0
Danq1101 <95583331+Danq1101@users.noreply.github.com>
2024-06-27 05:50:47 +0300

    Update CategoryController.java
    Cross

--- src/main/java/com/example/springPizza/web/controller/CategoryController.java
+++ src/main/java/com/example/springPizza/web/controller/CategoryController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/pizza/category")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CategoryController {
     private final CategoryService categoryService;
 
