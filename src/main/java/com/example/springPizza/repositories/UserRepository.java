b3198a0be415f66ae5e140dec4b5fe84aba88ece
Netharus <nekromant12331@gmail.com>
2024-07-04 20:20:44 +0300

    fixed conflict

--- src/main/java/com/example/springPizza/repositories/UserRepository.java
+++ src/main/java/com/example/springPizza/repositories/UserRepository.java
@@ -2,12 +2,14 @@ package com.example.springPizza.repositories;
 
 import com.example.springPizza.database.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     //TODO Вернуть как было заменить User на Optional
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
     Optional<User> findByUsernameOrEmail(String username, String email);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
